8f0be15f1db1aeba478e1c804fb1352b347e17fb
wxy <2639536908@qq.com>
2023-09-04 22:42:58 +0800

    fix 天津生态环境局

--- article_crawler/article_crawler/spiders/a天津生态环境局.py
+++ article_crawler/article_crawler/spiders/a天津生态环境局.py
@@ -10,4 +10,20 @@ class A天津生态环境局Spider(baseSpider):
             "content": response.css("div.TRS_PreAppend p::text").getall()
         }
 
+    @baseSpider.parser('天津生态环境局', 'https://sthj.tj.gov.cn')
+    def parser_2(self, response, **kwargs):
+        return {
+            "content": response.css("div.TRS_UEDITOR p::text").getall()
+        }
+
+    @baseSpider.parser('天津生态环境局', 'https://sthj.tj.gov.cn')
+    def parser_3(self, response, **kwargs):
+        return {
+            "content": response.css("#zoom p::text").getall()
+        }
+
+
+
+
+
 
